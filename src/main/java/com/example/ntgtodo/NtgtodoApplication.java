314d12008709eff352808653eeecd4d947fa578a
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-23 21:52:21 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/ntgtodo/NtgtodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ntgtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtgtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtgtodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ntgtodo/NtgtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ntgtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtgtodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
